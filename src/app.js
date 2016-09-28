93dc6c4c2a342c51e1308dd59cc568c0eb7d6a9d
Ben Griffiths <ben@atomist.com>
2016-09-28 11:03:11 +0100

    Implement everything :-)

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { TodoList } from './components';
-import { List, Map } from 'immutable';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+import { TodoList } from './containers';
 
-const dummyTodos = List([
-    Map({ id: 0, isDone: true,  text: 'make components cool' }),
-    Map({ id: 1, isDone: false, text: 'design actions' }),
-    Map({ id: 2, isDone: false, text: 'implement reducer' }),
-    Map({ id: 3, isDone: false, text: 'connect components' })
-]);
+const store = createStore(reducer);
 
 render(
-    <TodoList todos={dummyTodos} />,
+    <Provider store={store}>
+      <TodoList />
+    </Provider>,
     document.getElementById('app')
     );
